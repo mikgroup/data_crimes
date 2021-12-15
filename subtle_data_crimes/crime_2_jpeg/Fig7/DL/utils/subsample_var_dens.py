d52378ca594ad73157e823297883bb409f541a11
EfratShimron <efrat.joy@gmail.com>
2021-12-14 21:17:43 -0800

    updated Fig7/DL/utils/subsample_var_dens.py

--- subtle_data_crimes/crime_2_jpeg/Fig7/DL/utils/subsample_var_dens.py
+++ subtle_data_crimes/crime_2_jpeg/Fig7/DL/utils/subsample_var_dens.py
@@ -1,22 +1,14 @@
 """
 This code is based on the code subsample_fastmri.py by Facebook, Inc. and its affiliates
-The new additions include functions for variable-density sampling, which are based on Miki Lustig's l1-SPIRiT toolbox.
+We added functions for variable-density sampling, which are based on Miki Lustig's l1-SPIRiT toolbox.
 
 Efrat Shimron, UC Berkeley (2021)
 
 """
 
-import sys
-# for running on mikQNAP:
-sys.path.append("/mikQNAP/efrat/1_inverse_crimes/1_mirror_PyCharm_CS_MoDL_merged/SubtleCrimesRepo/")
-
-
-
 import numpy as np
 import torch
-import matplotlib.pyplot as plt
-from subtle_data_crimes.functions import gen_1D_var_dens_mask, gen_2D_var_dens_mask , genPDF,genSampling
-import matplotlib.pyplot as plt
+from subtle_data_crimes.functions.sampling_funcs import gen_1D_var_dens_mask, gen_2D_var_dens_mask , genPDF,genSampling
 
 
 class MaskFuncVarDens_1D:
@@ -49,14 +41,9 @@ class MaskFuncVarDens_1D:
                 uniformly each time. An acceleration of 4 retains 25% of the columns, but they may
                 not be spaced evenly.
         """
-        #if len(center_fractions) != len(accelerations):
-        #    raise ValueError('Number of center fractions should match number of accelerations')
 
-        #self.center_fractions = center_fractions
-        #self.accelerations = accelerations
         self.rng = np.random.RandomState()
         self.R = R
-        #self.calib = calib
         self.calib = calib*pad_ratio
         self.var_dens_flag = var_dens_flag
 
@@ -79,19 +66,6 @@ class MaskFuncVarDens_1D:
         self.rng.seed(seed)
         num_cols = shape[-2]
 
-        #choice = self.rng.randint(0, len(self.accelerations))
-        #center_fraction = self.center_fractions[choice]
-        #acceleration = self.accelerations[choice]
-
-        # Create the mask
-        # num_low_freqs = int(round(num_cols * center_fraction))
-        # prob = (num_cols / acceleration - num_low_freqs) / (num_cols - num_low_freqs)
-        #
-        # # the next code produces a 1D mask (i.e. a vector of False/True values indicated columns to be sampled)
-        # mask = self.rng.uniform(size=num_cols) < prob
-        # pad = (num_cols - num_low_freqs + 1) // 2
-        # mask[pad:pad + num_low_freqs] = True
-
         R = self.R
         calib = self.calib
 
@@ -104,11 +78,6 @@ class MaskFuncVarDens_1D:
                 poly_degree = 12
             elif self.var_dens_flag=='strong':
                 poly_degree = 6
-        elif R == 5:
-            if self.var_dens_flag == 'weak':
-                poly_degree = 5
-            #elif self.var_dens_flag == 'strong':
-                #TODO: implement this
         elif R == 4:
             if self.var_dens_flag == 'weak':
                 poly_degree = 10
@@ -119,13 +88,6 @@ class MaskFuncVarDens_1D:
         elif R == 2:
             poly_degree = 4
 
-        # # create a 1D pdf
-        # pdf = self.genPDF(num_cols, poly_degree, 1 / R)
-        # mask = self.genSampling(pdf, iter=10, tol=60, calib=calib)
-
-        ## create a 1D pdf
-        #pdf = genPDF(np.array([num_cols]), poly_degree, 1 / R)
-        #mask = genSampling(pdf, iter=50, tol=20,calib=calib )
 
         mask, pdf = gen_1D_var_dens_mask()
 
@@ -155,14 +117,9 @@ class MaskFuncVarDens_2D:
                 uniformly each time. An acceleration of 4 retains 25% of the columns, but they may
                 not be spaced evenly.
         """
-        # if len(center_fractions) != len(accelerations):
-        #    raise ValueError('Number of center fractions should match number of accelerations')
 
-        # self.center_fractions = center_fractions
-        # self.accelerations = accelerations
         self.rng = np.random.RandomState()
         self.R = R
-        # self.calib = calib
         self.calib = calib
         self.var_dens_flag = var_dens_flag
 
@@ -189,7 +146,6 @@ class MaskFuncVarDens_2D:
         imSize = np.array([num_rows,num_cols])
         mask, pdf, poly_degree_new = gen_2D_var_dens_mask(self.R, imSize, self.var_dens_flag,calib = self.calib)
 
-        # # debugging plot
         # fig = plt.figure()
         # plt.subplot(1,2,1)
         # plt.imshow(np.abs(pdf),cmap="gray")
